a402137f5ac4d09c4f8088b3c0d25fad8f495dd6
BlazPocrnja <bpocrnja@gmail.com>
2018-11-04 21:13:40 -0700

    Deployment changes.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CV from './App';
 import Data from './data';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(<CV {...Data} />, global.document.getElementById('root'));
-
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><CV {...Data} /></Router>, global.document.getElementById('root'));
